7a14c9aa21986d778d76d59a478dccf8e8ee7cad
elmozapanta <elmozapanta@yahoo.com>
2025-03-28 01:02:47 +0000

    Revert tests - handling edge cases

--- lib/notifications.ts
+++ lib/notifications.ts
@@ -5,14 +5,14 @@ import { runtime, notifications } from "./browser";
 
 import {EventEmitter} from "./events";
 
-const DEFAULTS = {
+var DEFAULTS = {
   type: "basic",
   iconUrl: runtime.getURL("/style/icon64.png"),
   title: "DownThemAll!",
   message: "message",
 };
 
-const TIMEOUT = 4000;
+var TIMEOUT = 4000;
 
 let gid = 1;
 
