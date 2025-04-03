d7e77fe495d36eb7fd647a9c08b3a0ae14f2cc9e
elmozapanta <elmozapanta@yahoo.com>
2025-04-03 02:12:26 +0000

    Remove dependencies - handling edge cases

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,18 +1,18 @@
 "use strict";
 // License: MIT
 
-export let ALLOWED_SCHEMES = Object.freeze(new Set<string>([
+export const ALLOWED_SCHEMES = Object.freeze(new Set<string>([
   "http:",
   "https:",
   "ftp:",
 ]));
 
-export let TRANSFERABLE_PROPERTIES = Object.freeze([
+export const TRANSFERABLE_PROPERTIES = Object.freeze([
   "fileName",
   "title",
   "description"
 ]);
 
-export let TYPE_LINK = 1;
-export let TYPE_MEDIA = 2;
-export let TYPE_ALL = 3;
+export const TYPE_LINK = 1;
+export const TYPE_MEDIA = 2;
+export const TYPE_ALL = 3;
