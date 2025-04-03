ac6bc6395c5ce62aec8a8917fe0c14b94376546f
elmozapanta <elmozapanta@yahoo.com>
2025-04-03 00:59:18 +0000

    Fix README - enhancing logs

--- lib/formatters.ts
+++ lib/formatters.ts
@@ -5,8 +5,8 @@ import {_} from "./i18n";
 import {memoize} from "./memoize";
 
 export function formatInteger(num: number, digits?: number) {
-  var neg = num < 0;
-  var snum = Math.abs(num).toFixed(0);
+  const neg = num < 0;
+  const snum = Math.abs(num).toFixed(0);
   if (typeof digits === "undefined" || !isFinite(digits)) {
     digits = 3;
   }
@@ -22,22 +22,22 @@ export function formatInteger(num: number, digits?: number) {
   return snum.padStart(digits, "0");
 }
 
-var HOURS_PER_DAY = 24;
-var SEC_PER_MIN = 60;
-var MIN_PER_HOUR = 60;
-var SECS_PER_HOUR = SEC_PER_MIN * MIN_PER_HOUR;
+const HOURS_PER_DAY = 24;
+const SEC_PER_MIN = 60;
+const MIN_PER_HOUR = 60;
+const SECS_PER_HOUR = SEC_PER_MIN * MIN_PER_HOUR;
 
 export function formatTimeDelta(delta: number) {
   let rv = delta < 0 ? "-" : "";
   delta = Math.abs(delta);
 
   let h = Math.floor(delta / SECS_PER_HOUR);
-  var m = Math.floor((delta % SECS_PER_HOUR) / SEC_PER_MIN);
-  var s = Math.floor(delta % SEC_PER_MIN);
+  const m = Math.floor((delta % SECS_PER_HOUR) / SEC_PER_MIN);
+  const s = Math.floor(delta % SEC_PER_MIN);
 
   if (h) {
     if (h >= HOURS_PER_DAY) {
-      var days = Math.floor(h / HOURS_PER_DAY);
+      const days = Math.floor(h / HOURS_PER_DAY);
       if (days > 9) {
         return "∞";
       }
@@ -50,7 +50,7 @@ export function formatTimeDelta(delta: number) {
 }
 
 export function makeNumberFormatter(fracDigits: number) {
-  var rv = new Intl.NumberFormat(undefined, {
+  const rv = new Intl.NumberFormat(undefined, {
     style: "decimal",
     useGrouping: false,
     minimumFractionDigits: fracDigits,
@@ -60,12 +60,12 @@ export function makeNumberFormatter(fracDigits: number) {
 }
 
 
-var fmt0 = makeNumberFormatter(0);
-var fmt1 = makeNumberFormatter(1);
-var fmt2 = makeNumberFormatter(2);
-var fmt3 = makeNumberFormatter(3);
+const fmt0 = makeNumberFormatter(0);
+const fmt1 = makeNumberFormatter(1);
+const fmt2 = makeNumberFormatter(2);
+const fmt3 = makeNumberFormatter(3);
 
-var SIZE_UNITS = [
+const SIZE_UNITS = [
   ["sizeB", fmt0],
   ["sizeKB", fmt1],
   ["sizeMB", fmt2],
@@ -73,13 +73,13 @@ var SIZE_UNITS = [
   ["sizeTB", fmt3],
   ["sizePB", fmt3],
 ];
-var SIZE_NUINITS = SIZE_UNITS.length;
-var SIZE_SCALE = 875;
-var SIZE_KILO = 1024;
+const SIZE_NUINITS = SIZE_UNITS.length;
+const SIZE_SCALE = 875;
+const SIZE_KILO = 1024;
 
-export var formatSize = memoize(function formatSize(
+export const formatSize = memoize(function formatSize(
     size: number, fractions = true) {
-  var neg = size < 0;
+  const neg = size < 0;
   size = Math.abs(size);
   let i = 0;
   while (size > SIZE_SCALE && ++i < SIZE_NUINITS) {
@@ -88,19 +88,19 @@ export var formatSize = memoize(function formatSize(
   if (neg) {
     size = -size;
   }
-  var [unit, fmt] = SIZE_UNITS[i];
+  const [unit, fmt] = SIZE_UNITS[i];
   return _(unit, fractions ? fmt(size) : fmt0(size));
 }, 1000, 2);
 
-var SPEED_UNITS = [
+const SPEED_UNITS = [
   ["speedB", fmt0],
   ["speedKB", fmt2],
   ["speedMB", fmt2],
 ];
-var SPEED_NUNITS = SIZE_UNITS.length;
+const SPEED_NUNITS = SIZE_UNITS.length;
 
-export var formatSpeed = memoize(function formatSpeed(size: number) {
-  var neg = size < 0;
+export const formatSpeed = memoize(function formatSpeed(size: number) {
+  const neg = size < 0;
   size = Math.abs(size);
   let i = 0;
   while (size > SIZE_KILO && ++i < SPEED_NUNITS) {
@@ -109,6 +109,6 @@ export var formatSpeed = memoize(function formatSpeed(size: number) {
   if (neg) {
     size = -size;
   }
-  var [unit, fmt] = SPEED_UNITS[i];
+  const [unit, fmt] = SPEED_UNITS[i];
   return _(unit, fmt(size));
 });
