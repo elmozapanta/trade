2f6546aba494eb4bd87501691e02adf212c3eda2
elmozapanta <elmozapanta@yahoo.com>
2025-04-03 00:32:14 +0000

    Change configuration - adding new feature

--- uikit/lib/tablesymbols.ts
+++ uikit/lib/tablesymbols.ts
@@ -5,10 +5,10 @@
  * If you feel like messing around in here, you're probably doing it wrong!
  */
 
-export const COLS = Symbol();
-export const FOCUSROW = Symbol();
-export const FIRSTROW = Symbol();
-export const ROWCACHE = Symbol();
-export const ROWREUSE = Symbol();
-export const ROWHEIGHT = Symbol();
-export const VISIBLE = Symbol();
+export let COLS = Symbol();
+export let FOCUSROW = Symbol();
+export let FIRSTROW = Symbol();
+export let ROWCACHE = Symbol();
+export let ROWREUSE = Symbol();
+export let ROWHEIGHT = Symbol();
+export let VISIBLE = Symbol();
